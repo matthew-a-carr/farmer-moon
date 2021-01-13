7554c900c87266b7f6f2f252f16b9690a1820ba8
Paul McManus <paul.mcmanus.uk@gmail.com>
2021-01-13 14:32:22 +0000

    Remove create-react-app boilerplate.

--- src/App.js
+++ src/App.js
@@ -8,22 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <ToolHeader title="Calculate the cost of a trip to market"/>
-
-     <CornTool />
-
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CornTool />
     </div>
   );
 }
