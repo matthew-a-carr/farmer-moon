ed2f5ad0198c891a876860eac97faf032a93d39d
Paul McManus <paul.mcmanus.uk@gmail.com>
2021-01-13 14:23:10 +0000

    Add logic to calculate the cost of the trip.

--- src/components/CornTool/index.js
+++ src/components/CornTool/index.js
@@ -1,12 +1,21 @@
 import React, {useState} from 'react';
 import QuantitySelector from '../QuantitySelector';
+import { calculateCostOfTrip } from '../../lib/cornCalculations';
 
 const CornTool = () => {
-    const [ numberOfBags, setNumberOfBags ] = useState(2);
+    const [ numberOfBags, setNumberOfBags ] = useState(0);
+
+    const [ costOfTrip, setCostOfTrip ] = useState(0);
+
+    const onChangeCornQuantity = (q) => {
+        setNumberOfBags(q);
+        setCostOfTrip(calculateCostOfTrip(q));
+    }
 
     return (
         <>
-            <QuantitySelector quantity={numberOfBags} onQuantityChange={setNumberOfBags} />
+            <QuantitySelector quantity={numberOfBags} onQuantityChange={onChangeCornQuantity} />
+            <p>{costOfTrip}</p>
         </>
     );
 }
