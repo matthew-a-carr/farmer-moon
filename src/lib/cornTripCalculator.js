b7d66818ee3771b711a00f0ada29f72c8622814f
Matthew Carr <matt.carr@madetech.com>
2021-01-14 13:36:37 +0000

    Update cornTripCalculator function to calculate trips for no corn and all geese

--- src/lib/cornTripCalculator.js
+++ src/lib/cornTripCalculator.js
@@ -3,21 +3,25 @@ export const cornConst = 'Corn';
 export const emptyConst = 'Nothing';
 
 export const cornTripCalculator = (numberOfBagsOfCorn, numberOfGeese) => {
-    if (numberOfBagsOfCorn == 1) {
-        return [cornConst];
-    } else if (numberOfBagsOfCorn > 1) {
-        const trips = [];
-
-        for (let i = 1; i <= numberOfBagsOfCorn; ++i) {
-            trips.push(cornConst);
-
-            if (i < numberOfBagsOfCorn) {
-                trips.push(emptyConst);
-            }
-        }
-
-        return trips;
+    if (numberOfBagsOfCorn >= 1) {
+        return calculateTripsForSingleItem(numberOfBagsOfCorn, cornConst);
+    } else if (numberOfGeese >= 1) {
+        return calculateTripsForSingleItem(numberOfGeese, geeseConst);
     }
 
     return [emptyConst];
+}
+
+const calculateTripsForSingleItem = (numberOfItems, typeOfItem) => {
+    const trips = [];
+
+    for (let i = 1; i <= numberOfItems; ++i) {
+        trips.push(typeOfItem);
+
+        if (i < numberOfItems) {
+            trips.push(emptyConst);
+        }
+    }
+
+    return trips;
 }
--- src/lib/cornTripCalculator.test.js
+++ src/lib/cornTripCalculator.test.js
@@ -1,4 +1,4 @@
-import { cornTripCalculator, emptyConst, cornConst } from './cornTripCalculator';
+import { cornTripCalculator, emptyConst, cornConst, geeseConst } from './cornTripCalculator';
 
 describe('The `cornTripCalculator` function', () => {
     const confirmTripSteps = (trips, ...steps) => {
@@ -9,32 +9,43 @@ describe('The `cornTripCalculator` function', () => {
         }
     }
 
-    it('should return 1 trip if you take no corn and no geese', () => {
+    it('should return 1 trip if you take 0 geese and 0 corn', () => {
         const trips = cornTripCalculator(0, 0);
         expect(trips.length).toBe(1);
         confirmTripSteps(trips, emptyConst);
     });
 
-    it('should return 1 trip if you take no geese and 1 corn', () => {
+    it('should return 1 trip if you take 0 geese and 1 corn', () => {
         const trips = cornTripCalculator(1, 0);
         expect(trips.length).toBe(1);
         confirmTripSteps(trips, cornConst);
     });
 
-    it('should return 3 trips if you take no geese and 2 corn', () => {
+    it('should return 3 trips if you take 0 geese and 2 corn', () => {
         const trips = cornTripCalculator(2, 0);
         expect(trips.length).toBe(3);
         confirmTripSteps(trips, cornConst, emptyConst, cornConst);
     });
 
-    it('should return return 5 trips if you take no geese and 3 corn', () => {
+    it('should return return 5 trips if you take 0 geese and 3 corn', () => {
         const trips = cornTripCalculator(3, 0);
         expect(trips.length).toBe(5);
         confirmTripSteps(trips, cornConst, emptyConst, cornConst, emptyConst, cornConst);
     });
 
-    it('should return 139 trips if you take no geese and 70 corn', () => {
+    it('should return 139 trips if you take 0 geese and 70 corn', () => {
         const trips = cornTripCalculator(70, 0);
         expect(trips.length).toBe(139);
-    })
+    });
+
+    it('should return 3 trips if you take 2 geese and 0 corn', () => {
+        const trips = cornTripCalculator(0, 2);
+        expect(trips.length).toBe(3);
+        confirmTripSteps(trips, geeseConst, emptyConst, geeseConst);
+    });
+
+    it('should return 139 trips if you take 70 geese and 0 corn', () => {
+        const trips = cornTripCalculator(0, 70);
+        expect(trips.length).toBe(139);
+    });
 });
